f25728d0486014b5364c318e6f53e7eecc140cbd
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-13 16:39:33 +0200

    fix: calcolo percentuale

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ let prezzoPerKm = 0.21;
 let prezzoTotale = chilometri * prezzoPerKm 
 
 if (eta < 18){
-    prezzoTotale = prezzoPerKm / 20 * 100
+    prezzoTotale = prezzoTotale - ( prezzoTotale * 0.2 )
  } else if (eta >= 65){
-    prezzoTotale = prezzoPerKm / 40 * 100
+    prezzoTotale = prezzoTotale - ( prezzoTotale * 0.4 )
  }
 
 prezzoTotale = prezzoTotale.toFixed(2)
