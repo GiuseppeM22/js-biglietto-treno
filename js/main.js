72051675255478e3adb17c44e25fc8dd9349f9ee
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-13 16:11:45 +0200

    feat: add 20 40%

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ let chilometri = prompt ("Quanti chilometri vuoi percorrere?")
 let eta = prompt ("Quanti anni hai?")
 let prezzoPerKm = 0.21;
 let prezzoTotale = chilometri * prezzoPerKm 
-alert (`il prezzo del tuo biglietto è ${prezzoTotale} $`)
 
 if (eta < 18){
     prezzoTotale = prezzoPerKm / 20 * 100
  } else if (eta >= 65){
-    prezzoTotale = prezzoPerKm / 20 * 100
+    prezzoTotale = prezzoPerKm / 40 * 100
  }
     
+alert (`il prezzo del tuo biglietto è ${prezzoTotale} $`)
+
  
 
